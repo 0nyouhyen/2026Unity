a12815b72e496b16566db46e2d637f3f147e26e5
0nyouhyen <zxfy12345@gmail.com>
2026-07-10 11:27:28 +0900

    연산자

--- Assets/Script/NEW1.cs
+++ Assets/Script/NEW1.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NEW1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] int age = 20;
     void Start()
     {
-        
+        //연산자
+        Debug.Log(10 + 3);
+        Debug.Log(10 - 3);
+        Debug.Log(10 * 3);
+        Debug.Log(10 / 3);
+        Debug.Log(10 % 3);
+        Debug.Log("나는 " + age + "살");
     }
 
     // Update is called once per frame
--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    int age = 20;
-    float height = 1.71f;
-    string name = "김민섭";
-    bool isStudent = true;
-
     // Start is called before the first frame update
+    [SerializeField] int age = 20;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
