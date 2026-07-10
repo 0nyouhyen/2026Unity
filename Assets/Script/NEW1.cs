2104bf622db369c9bdc8562124b40112055f9ca6
0nyouhyen <zxfy12345@gmail.com>
2026-07-10 11:41:13 +0900

    c#

--- Assets/Script/NEW1.cs
+++ Assets/Script/NEW1.cs
@@ -7,6 +7,7 @@ public class NEW1 : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] int age = 20;
+    int count = 1;
     void Start()
     {
             //연산자
@@ -16,7 +17,24 @@ public class NEW1 : MonoBehaviour
             Debug.Log(10 / 3);
             Debug.Log(10 % 3);
             Debug.Log("나는 " + age + "살");
+
+        if (age>=20)
+        {
+            Debug.Log("성인입니다.");
         }
+        else
+        {
+            Debug.Log("미성년자입니다. ");
+        }
+        for (int i = 0; i < 10; i++) ;
+        {
+            Debug.Log("Start 실행 후: " + i + "번 반복");
+        }
+        while (count <= 7)
+        {
+            Debug.Log("Whiel 반복 중: " + count + "번 반복");
+        }
+    }
 
 // Update is called once per frame
 void Update()
